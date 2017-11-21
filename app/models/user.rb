f422f4f4a3a29c41b062804f0a94589b27b511c4
Josh Anderson <joshua.frederick.anderson@gmail.com>
2017-11-21 21:24:22 +0000

    added user sign-in/sign-out

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
          
-         
   def avatar_url(size)
     gravatar_id = Digest::MD5::hexdigest(self.email).downcase
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
