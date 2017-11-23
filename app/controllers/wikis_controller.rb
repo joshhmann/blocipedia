48128ae1c3565f98abccc432a012e08574548550
Josh Anderson <joshua.frederick.anderson@gmail.com>
2017-11-23 05:53:02 +0000

    finished U and D part of CRUD

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -28,5 +28,33 @@ class WikisController < ApplicationController
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
+  end
+  
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+    @wiki.private = nil
+    
+    if @wiki.save
+      flash[:notice] = "Wiki page was updated successfully."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "Tehre was an error saving your page. Please try again."
+      render :edit
+    end
+  end
+  
+  def destroy
+    @wiki = Wiki.find(params[:id])
+    
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+      redirect_to wikis_path
+    else
+      flash.now[:alert] = "There was an error deleting the page."
+      render :show
+    end
   end
 end
--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -42,4 +42,64 @@ RSpec.describe WikisController, type: :controller do
      expect(assigns(:wiki)).to eq(my_wiki)
    end
  end
-end
+ 
+ describe "GET edit" do
+   it "returns http success" do
+     get :edit, {id: my_wiki.id}
+     expect(response).to have_http_status(:success)
+   end
+   
+   it "renders the #edit view" do
+     get :edit, {id: my_wiki.id}
+     expect(response).to render_template :edit
+   end
+   
+   it "assigns post to be updated to @wiki" do
+     get :edit, {id: my_wiki.id}
+     
+     wiki_instance = assigns(:wiki)
+     
+     expect(wiki_instance.id).to eq my_wiki.id
+     expect(wiki_instance.title).to eq my_wiki.title
+     expect(wiki_instance.body).to eq my_wiki.body
+     expect(wiki_instance.private).to eq my_wiki.private
+     expect(wiki_instance.user).to eq my_wiki.user
+   end
+ end
+ 
+ describe "PUT update" do
+   it "updates wiki with expected attributes" do
+     new_title = Faker::Overwatch.name
+     new_body = Faker::Overwatch.quote
+     
+     put :update, id: my_wiki.id, wiki: {title: new_title, body: new_body}
+     
+     updated_wiki = assigns(:wiki)
+     expect(updated_wiki.id).to eq my_wiki.id
+     expect(updated_wiki.title).to eq new_title
+     expect(updated_wiki.body).to eq new_body
+   end
+   
+   it "redirects to the updated wiki" do 
+     new_title = Faker::Overwatch.name
+     new_body = Faker::Overwatch.quote
+     
+     put :update, id: my_wiki.id, wiki: {title: new_title, body: new_body}
+     expect(response).to redirect_to my_wiki
+   end
+ end
+ 
+ describe "DELETE destroy" do
+   it "deletes the post" do
+     delete :destroy, {id: my_wiki.id}
+     
+     count = Wiki.where({id: my_wiki.id}).size
+     expect(count).to eq 0
+   end
+   
+   it "redirects to wiki index" do
+     delete :destroy, {id: my_wiki.id}
+     expect(response).to redirect_to wikis_path
+   end
+ end
+end 
