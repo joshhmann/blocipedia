a009e4fac7a65b6754e806431fabf83e532c3234
Josh Anderson <joshua.frederick.anderson@gmail.com>
2017-11-28 21:14:04 +0000

    added policies

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,18 +1,23 @@
 class WikisController < ApplicationController
+ before_action :authenticate_user!, except: [:index, :show]
   def index
     @wikis = Wiki.all
+    authorize @wikis
   end
 
   def show
     @wiki = Wiki.find(params[:id])
+    authorize @wiki
   end
 
   def new
     @wiki = Wiki.new
+    authorize @wiki
   end
   
   def create 
     @wiki = Wiki.new(wiki_params)
+    authorize @wiki
     
     if @wiki.save
       flash[:notice] = "Wiki page was saved successfully."
@@ -25,11 +30,13 @@ class WikisController < ApplicationController
 
   def edit
     @wiki = Wiki.find(params[:id])
+    authorize @wiki
   end
   
   def update
     @wiki = Wiki.find(params[:id])
     @wiki.assign_attributes(wiki_params)
+    authorize @wiki
     
     if @wiki.save
       flash[:notice] = "Wiki page was updated successfully."
@@ -42,6 +49,7 @@ class WikisController < ApplicationController
   
   def destroy
     @wiki = Wiki.find(params[:id])
+    authorize @wiki
     
     if @wiki.destroy
       flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -1,14 +1,2 @@
 class WikiPolicy < ApplicationPolicy
-  attr_reader :user, :wiki
-  
-  def initialize(user, wiki)
-    @user = user
-    @wiki = wiki
-  end
-
-  class Scope < Scope
-    def resolve
-      scope
-    end
-  end
 end
