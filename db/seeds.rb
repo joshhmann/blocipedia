23b5c7c8df57eb47cd5770e7ad90c69acaf16b31
Josh Anderson <joshua.frederick.anderson@gmail.com>
2017-11-29 04:59:24 +0000

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# create users
+require 'faker'
+
+5.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+    )
+end
+
+users = User.all
+    
+20.times do
+  Wiki.create!(
+    user: users.sample,
+    title: Faker::Witcher.character,
+    body: Faker::Witcher.quote,
+    private: false
+    )
+end
+
+puts "Seeds finished!"
+puts "#{User.count} users created!"
+puts "#{Wiki.count} wikis created!"
