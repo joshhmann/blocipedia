e6c6d5051ba49769424f8ad28f1290b38d0505a9
Josh Anderson <joshua.frederick.anderson@gmail.com>
2017-12-05 21:28:25 +0000

    Added callbacks, fixed roles upgrading/downgrading

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,16 @@ class UsersController < ApplicationController
   def show
     @user = current_user
   end
+  
+  
+  def downgrade
+    if current_user.premium?
+      current_user.standard!
+      flash[:notice] = "You have been downgraded to standard."
+      redirect_to root_path
+    else
+      flash[:error] = "There was an error downgrading your account. Please try again."
+      redirect_to edit_user_registration_path
+    end
+  end
 end
